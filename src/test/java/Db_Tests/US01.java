81eb39d0d9c0cdee4b61c67d83ae1a292022d1d9
pinarcankaya <pinarunalcan50@gmail.com>
2021-05-18 00:53:59 +0200

    framework_installation

--- src/test/java/Db_Tests/US01.java
+++ src/test/java/Db_Tests/US01.java
@@ -1,4 +1,27 @@
 package Db_Tests;
 
+import org.junit.Test;
+import utilities.DatabaseConnector;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class US01 {
+
+    List<Map<String, String>> list1 = new ArrayList<>();
+
+
+    @Test
+    public void soru01() throws SQLException {
+
+        String query1 = "select *\n" +
+                "from SIRKETLER";
+        list1= DatabaseConnector.getQueryAsAListOfMaps(query1);
+        System.out.println(list1);
+
+
+
+    }
 }
